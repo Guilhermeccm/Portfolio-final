fb18bdc5db6f40458fab417fb7f3a614f7a52a49
Guilhermeccm <guilhermematosn1@gmail.com>
2023-05-07 21:41:28 +0100

    Finished the nav and header. starting on the about

---
+++ portfolio/portfolio/src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './footer.css';
+function Footer() {
+  return (
+    <section id='footer'>footer</section>
+  )
+}
+
+export default Footer
---
+++ portfolio/portfolio/src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './testimonials.css';
+function Testimonials() {
+  return (
+    <section id='testimonials'>TESTIMONIALS</section>
+  )
+}
+
+export default Testimonials
