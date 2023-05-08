6e21a1dd4208009b7c5afe315be4ee70ffa2269e
Guilhermeccm <guilhermematosn1@gmail.com>
2023-05-08 21:22:45 +0100

    Project Finished

--- portfolio/portfolio/src/components/contact/Contact.jsx
+++ portfolio/portfolio/src/components/contact/Contact.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
 import './contact.css';
+import {MdOutlineEmail} from 'react-icons/md'
+import {BsWhatsapp} from 'react-icons/bs'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-function contact() {
+function Contact() {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_lvki2vw', 'template_sr1hd0r', form.current, 'OE9DADAGNeUgHwmu7')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  };
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className="contact__options">
+          {/* EMAIL */}
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>guilhermematosn1@gmail.com</h5>
+            <a href="mailto:guilhermematosn1@gmail.com" target="_blank" rel="noreferrer">Send an email</a>
+          </article>
+          {/* WHATSAPP */}
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>LinkedIn</h4>
+            <h5>+351 925430829</h5>
+            <a href="https://wa.me/+351925430829" target="_blank" rel="noreferrer">Send a message on WhatsApp</a>
+          </article>
+
+          
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit ={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" id="" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
-export default contact
+export default Contact
--- portfolio/portfolio/src/components/footer/Footer.jsx
+++ portfolio/portfolio/src/components/footer/Footer.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 import './footer.css';
+import {FaFacebookF} from 'react-icons/fa'
+import {FaInstagram} from 'react-icons/fa'
+import {FaTwitter} from 'react-icons/fa'
+
+
+
 function Footer() {
   return (
-    <section id='footer'>footer</section>
+    <footer>
+      <a href="#" className='footer__logo'>Guilherme</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+
+      <div className="footer__socials">
+        <a href="https://facebook.com" target="_blank"><FaFacebookF/></a>
+        <a href="https://instagram.com" target="_blank"><FaInstagram/></a>
+        <a href="https://twitter.com" target="_blank"><FaTwitter/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Guilherme Matos. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
